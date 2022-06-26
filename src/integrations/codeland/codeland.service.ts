1052454f4888b40a58df2fad67180159058096b8
DanielDDHM <daniel.ddhm@gmail.com>
2022-06-26 18:17:00 -0300

    fix:app-env

--- src/integrations/codeland/codeland.service.ts
+++ src/integrations/codeland/codeland.service.ts
@@ -4,9 +4,9 @@ import { StatusCode } from '../../constants/statuscode.typings';
 
 const apiCodelandService = axios.create({
   baseURL: (
-    process.env.APP_ENV?.toLowerCase() === 'prod'
+    process.env.NODE_ENV?.toLowerCase() === 'prod'
       ? BASE_URLS.CODELAND_SERVICE.PROD : (
-        process.env.APP_ENV?.toLowerCase() === 'nonprod'
+        process.env.NODE_ENV?.toLowerCase() === 'nonprod'
           ? BASE_URLS.CORE.DEV : BASE_URLS.CORE.TEST)
   ),
   validateStatus: (status: number) => status < StatusCode.INTERNAL_SERVER_ERROR,
