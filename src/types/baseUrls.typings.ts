2e86e3bf80dddef7fb84c218fee967ffb61c3c1a
DanielDDHM <daniel.ddhm@gmail.com>
2022-06-26 07:27:08 -0300

    chore: adjust req type

---
+++ src/types/baseUrls.typings.ts
@@ -0,0 +1,15 @@
+export const BASE_URLS = {
+  CORE: {
+    DEV: 'http://localhost:3000',
+    TEST: 'http://localhost:4000'
+  },
+  HR_SERVICE: {
+    PROD: ''
+  },
+  BET_SERVICE: {
+    PROD: ''
+  },
+  CODELAND_SERVICE: {
+    PROD: ''
+  }
+}
